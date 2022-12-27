c16c4e6a03189f0c9e1acfbe6c0283e2f81b4d41
Sean OReilly <soreilly424@gmail.com>
2022-12-27 15:18:28 -0600

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Sean Patrick Studios</title>
+        <meta name='description' content="Sean Patrick O'Reilly's Personal Project" />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main>
+        hello world
+      </main>
+    </>
+  )
+}
